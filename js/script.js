e3ace78d2183a242fa4d71a07ec08a19d21e75f7
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-03-17 22:02:55 +0530

    smooth scrolling

--- js/script.js
+++ js/script.js
@@ -497,3 +497,15 @@
 
 
 })(window.jQuery);
+
+
+  // Smooth scrolling
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
